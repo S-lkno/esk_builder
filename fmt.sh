7a962418980178be4e89c2eb82f2e8516fb28cce
bachnxuan <bachnxuan@gmail.com>
2026-02-09 20:57:15 +0700

    style(fmt): format the formatter
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- fmt.sh
+++ fmt.sh
@@ -2,10 +2,13 @@
 set -euo pipefail
 
 # Check for shfmt command
-command -v shfmt >/dev/null || { echo "shfmt not found"; exit 127; }
+command -v shfmt > /dev/null || {
+    echo "shfmt not found"
+    exit 127
+}
 
 # Get script directory
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd -P)"
 
 # Format build.sh with shfmt
-exec shfmt -w -i 4 -ci -bn -sr "$SCRIPT_DIR/build.sh"
+exec shfmt -w -i 4 -ci -bn -sr "$SCRIPT_DIR"/*.sh
